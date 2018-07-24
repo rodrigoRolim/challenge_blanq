7e3a9a3bf241139db9794a49f3d2808aa4c20a94
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-24 16:47:00 -0300

    import components of vue-material that really at usage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import Vue from 'vue';
-
+import { 
+  MdButton,
+  MdContent,
+  MdTabs,
+  MdTable } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
 import App from './App.vue';
 
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+Vue.use(MdTable)
 new Vue({
   el: '#app',
   render: h => h(App)
