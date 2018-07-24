764ff2d72df5b3caf1d005f23c58109b7555cc48
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-24 16:14:35 -0300

    create major component

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <h1>{{ message }}</h1>
+  <ComponentA></ComponentA>
+</div>
+ 
+</template>
+
+<script>
+import ComponentA from './components/componentA.vue';
+export default {
+  name: 'app',
+  components: {
+   ComponentA
+  },
+  data() {
+    return {
+      message: 'Hello, Vue!'
+    };
+  }
+};
+</script>
+<style lang="scss">
+ $font-stack:    Helvetica, sans-serif;
+ $primary-color: rgb(194, 2, 2);
+
+ body {
+  font: 100% $font-stack;
+  color: $primary-color;
+ }
+</style>
