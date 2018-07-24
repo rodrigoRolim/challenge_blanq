f228e934e3026b676cd4b7f2623364bb333cacf3
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-24 18:07:21 -0300

    created NavBar component for Business component

---
+++ src/components/empresas/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+ <nav>
+   <md-toolbar class="md-primary">
+     <h3 class="md-title">Primary</h3>
+   </md-toolbar>
+ </nav>
+</template>
+<script>
+ export default {
+  name: 'navbar'
+ }
+</script>
+<style lang="scss">
+
+</style>
+
