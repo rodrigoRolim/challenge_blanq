275fb783cd9e893144957bef41a56e5ab14fdbba
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 11:42:50 -0300

    added new route for NovaPessoa Component

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import App from './App.vue'
 import Empresas from './components/empresas/EmpresaLista.vue'
 import NovaEmpresa from './components/empresas/NovaEmpresa.vue'
-import Persons from './components/pessoas/PessoasLista.vue'
+import Pessoas from './components/pessoas/PessoasLista.vue'
+import NovaPessoa from './components/pessoas/NovaPessoa.vue'
 export default [
   { path: '/', redirect: '/business' },
   { path: '/business', component: Empresas},
   { path: '/new-business', component: NovaEmpresa },
-  { path: '/persons', component: Persons}
+  { path: '/persons', component: Pessoas },
+  { path: '/new-persons', component: NovaPessoa}
 ]
