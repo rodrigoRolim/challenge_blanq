fb2f5ceae08a9f12c45f2bebf5c7aafe5c6f9161
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 11:45:04 -0300

    bronken  very large md-table-cell tag  line

--- src/components/pessoas/PessoasLista.vue
+++ src/components/pessoas/PessoasLista.vue
@@ -14,7 +14,8 @@
         <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
         <md-table-cell md-label="Nome" md-sort-by="name">{{ item.name }}</md-table-cell>
         <md-table-cell md-label="Email" md-sort-by="email">{{ item.email }}</md-table-cell>
-        <md-table-cell md-label="Detalhes" md-sort-by="detail"><a href="">{{ item.detail }}</a></md-table-cell>
+        <md-table-cell md-label="Detalhes" md-sort-by="detail">
+         <a href="">{{ item.detail }}</a></md-table-cell>
       </md-table-row>
    </md-table>
  </div>  
