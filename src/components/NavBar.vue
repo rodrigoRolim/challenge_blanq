9b8a292e13fdf8f4f4ab8c77de0181b0333c03ae
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 10:55:13 -0300

    refactor navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+ <nav>
+   <md-toolbar class="md-primary">
+     <h3 class="md-title">Challenge Blanq</h3>
+     <div class="btn-nav">
+       <router-link to="/business"> 
+        <md-button class="md-raised" id="btn-bus">Empresas</md-button>
+      </router-link>
+      <router-link to="/staff"> 
+        <md-button class="md-raised">Pessoas</md-button>
+      </router-link>
+     </div>
+      
+   </md-toolbar>
+ </nav>
+</template>
+<script>
+ export default {
+  name: 'navbar'
+ }
+</script>
+<style lang="scss">
+ #btn-bus {
+   background-color:rgb(12, 136, 105);
+   color: white;
+ }
+ .btn-nav {
+   margin-left: 10px;
+ }
+ h3 {
+   margin-right: 30px;
+ }
+</style>
+
