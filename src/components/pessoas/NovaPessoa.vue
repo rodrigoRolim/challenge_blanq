9e62417c23b0c01b88fee385e6a519cf286dcf80
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 11:53:09 -0300

    alterd position for inputs

--- src/components/pessoas/NovaPessoa.vue
+++ src/components/pessoas/NovaPessoa.vue
@@ -1,52 +1,52 @@
 <template>
-  <div>
-   <NavBar></NavBar>
- <form class="md-layout  md-alignment-space-around-center">
-  <md-card class="md-layout-item md-size-50 md-small-size-100">
-   <md-card-header>
-    <div class="md-title">Pessoas</div>
-   </md-card-header>
-   <md-card-content>
-    <div class="md-layout md-gutter">
-      <div class="md-layout-item md-small-size-100">
-       <md-field>
-        <label for="person-firstname">nome</label>
-        <md-input 
-         name="person-firstname" 
-         id="person-firstname"></md-input>
-       </md-field>
-      </div> 
-      <div class="md-layout-item md-small-size-100">
-       <md-field>
-        <label for="person-lastname">sobrenome</label>
-        <md-input 
-         name="person-lastname" 
-         id="person-lastname"></md-input>
-       </md-field>
-      </div>
-      <div class="md-layout-item md-large-size-50">
-       <md-field>
-        <label for="person-email">email</label>
-        <md-input 
-         name="person-email" 
-         id="person-email"></md-input>
-       </md-field>
-     
-      </div>
-      <div class="md-layout-item md-small-size-100">
-       <md-field>
-        <label for="list-business">lista de empresas</label>
-        <md-input 
-         name="list-business" 
-         id="list-business"></md-input>
-       </md-field>
-      </div>
-    </div> 
-   </md-card-content>
-   <md-button>Save</md-button>
-  </md-card>
- </form> 
-  </div>
+ <div>
+  <NavBar></NavBar>
+  <form class="md-layout  md-alignment-space-around-center">
+   <md-card class="md-layout-item md-size-50 md-small-size-100">
+    <md-card-header>
+     <div class="md-title">Pessoas</div>
+    </md-card-header>
+    <md-card-content>
+     <div class="md-layout md-gutter">
+       <div class="md-layout-item md-small-size-100">
+        <md-field>
+         <label for="person-firstname">nome</label>
+         <md-input 
+          name="person-firstname" 
+          id="person-firstname"></md-input>
+        </md-field>
+       </div> 
+       <div class="md-layout-item md-small-size-100">
+        <md-field>
+         <label for="person-lastname">sobrenome</label>
+         <md-input 
+          name="person-lastname" 
+          id="person-lastname"></md-input>
+        </md-field>
+       </div>
+       <div class="md-layout-item md-large-size-50">
+        <md-field>
+         <label for="person-email">email</label>
+         <md-input 
+          name="person-email" 
+          id="person-email"></md-input>
+        </md-field>
+      
+       </div>
+       <div class="md-layout-item md-large-size-50">
+        <md-field>
+         <label for="list-business">lista de empresas</label>
+         <md-input 
+          name="list-business" 
+          id="list-business"></md-input>
+        </md-field>
+       </div>
+     </div> 
+    </md-card-content>
+    <md-button>Save</md-button>
+   </md-card>
+  </form> 
+ </div>
 </template>
 <script>
  
