5e23bad52866d9a187c181ab167120c654df15a1
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 18:11:28 -0300

    refactored names of routes

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,9 @@ import NovaEmpresa from './components/empresas/NovaEmpresa.vue'
 import Pessoas from './components/pessoas/PessoasLista.vue'
 import NovaPessoa from './components/pessoas/NovaPessoa.vue'
 export default [
-  { path: '/', redirect: '/business' },
-  { path: '/business', component: Empresas},
-  { path: '/new-business', component: NovaEmpresa },
-  { path: '/persons', component: Pessoas },
-  { path: '/new-persons', component: NovaPessoa}
+  { path: '/', redirect: '/company' },
+  { path: '/company', component: Empresas},
+  { path: '/new-company', component: NovaEmpresa },
+  { path: '/people', component: Pessoas },
+  { path: '/new-people', component: NovaPessoa}
 ]
