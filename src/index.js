13ba1d3197d1aaf3261a863c64794621d575920f
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 09:37:30 -0300

    added all components of md

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 import Vue from 'vue';
-import { 
-  MdButton,
-  MdContent,
-  MdTabs,
-  MdTable,
-  MdRipple,
-  MdCard,
-  MdNumeric,
-  MdToolbar,
-  MdIcon,
- } from 'vue-material/dist/components'
+
+import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import VueRouter from 'vue-router'
 import routes from  './routes'
 import EmpresaLista from './components/empresas/EmpresaLista.vue'
 import App from './App.vue'
 
-Vue.use(MdButton)
-Vue.use(MdContent)
-Vue.use(MdTabs)
-Vue.use(MdTable)
-Vue.use(MdCard)
-Vue.use(MdRipple)
-Vue.use(MdToolbar)
-Vue.use(MdIcon)
-
+Vue.use(VueMaterial)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 new Vue({
