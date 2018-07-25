c899513a24d3ecb312e0bc8adec50b337e9b5d87
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 16:07:01 -0300

    created phonelist component and added propos

---
+++ src/components/empresas/PhoneList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="lista-phone">
+     <md-list>
+      <md-list-item v-for="phone in form.phones" v-bind:key="phone">
+        {{ phone }} <span v-on:click="removePhone(phone)"><md-icon >delete_forever</md-icon></span>
+        </md-list-item>
+     </md-list>
+   </div> 
+</template>
+<script>
+ export default {
+  props: ['phoneList'],
+  name: 'PhoneList',
+  data: () => {
+   return {
+
+   }
+  }
+ }
+</script>
+<style>
+
+</style>
