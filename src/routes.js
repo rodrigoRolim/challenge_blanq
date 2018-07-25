da39ffd207a9ce1c2dd9ffc8ed317e61fd16813d
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 11:14:38 -0300

    added persons route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import App from './App.vue'
 import Empresas from './components/empresas/EmpresaLista.vue'
 import NovaEmpresa from './components/empresas/NovaEmpresa.vue'
-
+import Persons from './components/pessoas/PessoasLista.vue'
 export default [
   { path: '/', redirect: '/business' },
   { path: '/business', component: Empresas},
-  { path: '/new-business', component: NovaEmpresa }
+  { path: '/new-business', component: NovaEmpresa },
+  { path: '/persons', component: Persons}
 ]
