32e0efd192c92959e96a801847ec533a88e56c5b
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 16:47:33 -0300

    adde methods of picturelist

---
+++ src/components/empresas/PictureList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="lista-picture">
+     <md-list>
+      <md-list-item v-for="picture in form.pictures" v-bind:key="picture">
+        {{ picture }} <span v-on:click="removePicture()"><md-icon >delete_forever</md-icon></span>
+      </md-list-item>
+     </md-list>
+   </div> 
+</template>
+<script>
+ export default {
+  name: 'PictureList',
+  props: ['listPicture'],
+  data: () => {
+   return {
+
+   }
+  },
+  methods: {
+    addPictures () {
+      this.form.pictures.push(this.picture);
+    },
+    removePicture () {
+
+    },
+  }
+ }
+</script>
+<style>
+
+</style>
+
