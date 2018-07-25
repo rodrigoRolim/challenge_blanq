92bf7de803ecc81c3b97d7f9f635e44498e92e97
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 18:41:00 -0300

    added sendSave function

--- src/components/empresas/NovaEmpresa.vue
+++ src/components/empresas/NovaEmpresa.vue
@@ -1,7 +1,7 @@
 <template>
  <div>
   <NavBar></NavBar>
- <form novalidate class="md-layout  md-alignment-space-around-center">
+ <form novalidate class="md-layout  md-alignment-space-around-center" @submit="sendSave">
   <md-card class="md-layout-item md-size-50 md-small-size-100">
    <md-card-header>
     <div class="md-title">Empresas</div>
@@ -50,7 +50,19 @@
      </div>
     </div> 
    </md-card-content>
-   <md-button>Save</md-button>
+   <div class="listas">
+     <md-list class="lista-left">
+      <md-list-item v-for="phone in form.phones" v-bind:key="phone">
+        {{ phone }} <span v-on:click="removePhone(phone)"><md-icon >delete_forever</md-icon></span>
+        </md-list-item>
+     </md-list>
+     <md-list class="lista-right">
+      <md-list-item v-for="picture in form.pictures" v-bind:key="picture">
+        {{ picture }} <span v-on:click="removePicture(picture)"><md-icon >delete_forever</md-icon></span>
+      </md-list-item>
+     </md-list>
+   </div> 
+   <md-button v-on:click="sendSave">Save</md-button>
   </md-card>
  </form> 
  </div>
@@ -67,10 +79,10 @@
   data: () => {
    return {
     form: {
-        businessName: null,
-        businessAddress: null,
-        pictures: [],
-        phones: [],
+      name: null,
+      address: null,
+      pictures: [],
+      phones: [],
     },
     phone: '',
     picture: '',
@@ -82,19 +94,55 @@
       this.form.phones.push(this.phone);
     },
     addPictures () {
-      this.form.pictures.push(this.pictures);
+      this.form.pictures.push(this.picture);
+    },
+    removePhone (phone) {
+      this.form.phones = this.form.phones.filter(ph => ph !== phone)
+    },
+    removePicture (picture) {
+      this.form.pictures = this.form.pictures.filter(pic => pic !== picture)
     },
     sendSave () {
-
-    }
-  },
+      this.business = this.form;
+      console.log(this.business.name)
+      fetch('https://parseapi.back4app.com/classes/Company/', {
+        method: 'post',
+        headers: {         
+          "X-Parse-Application-Id": "JPdleQSgMjUF06VvAPfjPb6tyPwnDpepAeTEtBYL",         
+          "X-Parse-REST-API-Key": "eQM22TzI3BwImu6IVKXOeFei2NTLV6StBQvsUVJG"     
+        },
+        body: {
+          "name": "Company Test",   
+          "address":"Avenida Ayrton Senna da Silva, 200",         
+          "phones":["(43) 9999-9999"], 
+          "pictures": ["https://cdn.pixabay.com/photo/2018/06/30/09/29/music3507317_960_720.jpg"],         
+          "who_visited": ["AsPVqGwpqQ"] 
+        }
+      }).then(response =>{ 
+          response.json().then(json => {
+            console.log('check it out');
+            console.log(json)
+          })
+        })
+      },
+    },
   beforeDestroy () {
    this.business = null
    delete this.business
   },
  }
 </script>
-<style>
+<style lang="scss">
+.listas {
+  display: flex;
+  flex-direction: row;
+}
+.lista-right {
+  width: 50%;
+}
+.lista-left {
+  width: 50%;
+}
 form {
  padding: 100px 0;
 }
