315e433f6da7482af801af6cf3fc70986dc00651
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-25 10:54:54 -0300

    refactored routes for redirect url to business  wheter url macht with /

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Empresas from './components/empresas/EmpresaLista.vue'
 import NovaEmpresa from './components/empresas/NovaEmpresa.vue'
 
 export default [
-  { path: '/', component: Empresas },
+  { path: '/', redirect: '/business' },
+  { path: '/business', component: Empresas},
   { path: '/new-business', component: NovaEmpresa }
 ]
