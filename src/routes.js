a665e1660563ba9e687544bbd3c69c4c34e62843
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-26 00:42:03 -0300

    added route to companydetail

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ import Empresas from './components/empresas/EmpresaLista.vue'
 import NovaEmpresa from './components/empresas/NovaEmpresa.vue'
 import Pessoas from './components/pessoas/PessoasLista.vue'
 import NovaPessoa from './components/pessoas/NovaPessoa.vue'
+import CompanyDetail from './components/empresas/CompanyDetails.vue'
+
 export default [
   { path: '/', redirect: '/company' },
-  { path: '/company', component: Empresas},
+  { path: '/company', component: Empresas },
   { path: '/new-company', component: NovaEmpresa },
   { path: '/people', component: Pessoas },
-  { path: '/new-people', component: NovaPessoa}
+  { path: '/new-people', component: NovaPessoa },
+  { path: '/company/:id', component: CompanyDetail }
 ]
