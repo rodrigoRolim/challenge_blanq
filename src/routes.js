e855490a450ea38f54e9fac4c2cf9baf67a39222
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-30 10:26:00 -0300

    rename a fews components

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import App from './App.vue'
-import Companies from './components/empresas/CompanyList.vue'
+import CompanyList from './components/empresas/CompanyList.vue'
 import NewCompany from './components/empresas/NewCompany.vue'
-import People from './components/pessoas/PeopleList.vue'
+import PeopleList from './components/pessoas/PeopleList.vue'
 import NewPeople from './components/pessoas/NewPeople.vue'
 import CompanyDetail from './components/empresas/CompanyDetails.vue'
 import PeopleDetail from './components/pessoas/PeopleDetail.vue'
@@ -9,10 +9,10 @@ import NotFoundComponent from './components/NotFoundComponent.vue'
 
 export default [
   { path: '/', redirect: '/companies' },
-  { path: '/companies', name: 'company',  component: Companies },
+  { path: '/companies', name: 'company',  component: CompanyList },
   { path: '/new-company', name: 'newcompany', component: NewCompany },
   { path: '/new-company/:id', name: 'updatecompany', component: NewCompany},
-  { path: '/peoples', name: 'people', component: People },
+  { path: '/peoples', name: 'people', component: PeopleList },
   { path: '/new-people', name: 'newpeople', component: NewPeople },
   { path: '/new-people/:id', name: 'updatepeople', component: NewPeople },
   { path: '/people/:id', name: 'peopledetail', component: PeopleDetail },
